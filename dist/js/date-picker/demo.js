bfc67c6f74059e29b5279a365853cea041b8ace5
joux11 <caizajoe@gmail.com>
2023-01-06 18:30:39 -0500

    inicio proyecto

---
+++ dist/js/date-picker/demo.js
@@ -0,0 +1,12 @@
+(function($){
+    $(function(){
+
+        $('#id_4').datetimepicker({
+            "allowInputToggle": true,
+            "showClose": true,
+            "showClear": true,
+            "showTodayButton": true,
+            "format": "MM/DD/YYYY",
+        });
+    });
+})(jQuery);
